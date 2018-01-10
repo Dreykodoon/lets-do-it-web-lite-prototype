c0976e80b223c9c4c621ba226b52e39ae15a6133
Mihai <ciobanmihai@gmail.com>
2018-01-10 11:45:26 +0200

    Centered camera and added button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import Webcam from 'react-webcam';
 
+const styles = {
+  container: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '50%',
+    height: '500px',
+  },
+  camera: {
+    position: 'absolute',
+  },
+  button: {
+    position: 'relative',
+    top: '80%',
+    left: '50%',
+    width: '100px',
+    marginLeft: '-50px'
+
+  },
+};
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <Webcam audio={false}/>
+      <div style={styles.container}>
+        <Webcam style={styles.camera} audio={false}/>
+        <button style={styles.button}>Click me</button>
       </div>
     );
   }
